c98c46090890d02f5f39fe350b7ae469941fedea
RakeshHome <rakeshsondagar@gmail.com>
2024-02-01 01:11:01 +0530

    frontend

---
+++ src/Components/DiscriptionBox/DiscriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './DiscriptionBox.css'
+const DiscriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+     
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+            Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+            Reviews (123)
+        </div>
+      </div>
+    <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quo tenetur praesentium. Tempora expedita magni sequi itaque nobis necessitatibus nostrum officia deleniti dolores. Magnam atque neque fugiat nihil. Optio commodi, pariatur asperiores maxime ipsa, unde cum quos architecto nesciunt quisquam rem, ea quibusdam blanditiis aut!</p>
+      
+        <p>Magnam atque neque fugiat nihil. Optio commodi, pariatur asperiores maxime ipsa, unde cum quos architecto nesciunt quisquam rem, ea quibusdam blanditiis aut!</p>
+
+    </div>
+
+
+
+    </div>
+  )
+}
+
+export default DiscriptionBox
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartItems from '../Components/CartItems/CartItems'
+
+const Cart = () => {
+  return (
+    <React.Fragment>
+      <CartItems/>
+    </React.Fragment>
+  )
+}
+
+export default Cart
